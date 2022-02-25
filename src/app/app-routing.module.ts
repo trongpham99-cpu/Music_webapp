a1482f7717f1ad4ebab081eab451c0b152acfb90
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-02-25 21:07:19 +0700

    profile - detailmusic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Detail', loadChildren: () => import('./Quy/Pages/detailmusic/detailmusic.module').then(m => m.DetailmusicModule) }, 
+    { path: 'Profile', loadChildren: () => import('./Quy/Pages/profile/profile.module').then(m => m.ProfileModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
