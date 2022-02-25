196f42fb3a30c2962c1ac9dffd7c59b09f49c6cf
DucDatt <dat.hd3745@sinhvien.hoasen.edu.vn>
2022-02-25 17:03:38 +0700

    Home

---
+++ src/app/Dat/Components/kind-of-music/kind-of-music.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kind-of-music',
+  templateUrl: './kind-of-music.component.html',
+  styleUrls: ['./kind-of-music.component.scss']
+})
+export class KindOfMusicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
