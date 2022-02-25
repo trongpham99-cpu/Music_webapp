15f34d13803a63ce8afbeb6ae17a94d68253bd29
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-02-25 17:01:18 +0700

    Profile & detail music

---
+++ src/app/Quy/Pages/detailmusic/detailmusic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detailmusic',
+  templateUrl: './detailmusic.component.html',
+  styleUrls: ['./detailmusic.component.scss']
+})
+export class DetailmusicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Detailmusic', loadChildren: () => import('./Quy/Pages/detailmusic/detailmusic.module').then(m => m.DetailmusicModule) }, { path: 'Profile', loadChildren: () => import('./Quy/Pages/profile/profile.module').then(m => m.ProfileModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
