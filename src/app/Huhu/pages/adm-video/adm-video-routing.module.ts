f86e22cb39d8ea713955c0e24eea1af277e970bd
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-03-03 11:32:00 +0700

    Projct nhóm

--- src/app/Huhu/pages/adm-video/adm-video-routing.module.ts
+++ src/app/Huhu/pages/adm-video/adm-video-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminVideoManageComponent } from '../../components/admin-video-manage/admin-video-manage.component';
 import { AdmVideoComponent } from './adm-video.component';
 
-const routes: Routes = [{ path: '', component: AdmVideoComponent }];
+const routes: Routes = [{ path: '', component: AdmVideoComponent,
+// children: [
+//    { path: 'video', component:  AdminVideoManageComponent}
+// ]
+ }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
