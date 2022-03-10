ba7e373498ea648f92a63df53797718c7cc292d3
Trong Pham <trong.ptd3604@sinhvien.hoasen.edu.vn>
2022-03-10 17:24:49 +0700

    http-errors

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const createError = require('http-errors')
 const server = express()
 server.use(cors());
 server.use(bodyParser.json());
@@ -12,5 +13,15 @@ const audioRouter = require("./routers/audio.js");
 server.use("/user",userRouter);
 server.use("/api", exampleRouter);
 server.use("/audio", audioRouter);
+
+server.use((req, res, next)=>{
+    next(createError.NotFound(`This route does not exits`))
+})
+server.use((err, req, res, next)=>{
+    res.json({
+        status: err.status || 500,
+        message: err.message
+    })
+})
 
 module.exports = server;
