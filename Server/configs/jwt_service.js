cdd4f797da743745ac8a5ee9db3b8f12b827ce75
HuynhHuuu <HUU.H9155@sinhvienhoasen.edu.vn>
2022-03-12 12:07:38 +0700

    BugHUU

--- Server/configs/jwt_service.js
+++ Server/configs/jwt_service.js
@@ -1,11 +1,12 @@
 const JWT = require('jsonwebtoken');
-
+const key = require("./key");
+const createError = require('http-errors');
 const signAccessToken = async (userID)=>{
     return new Promise ( (resolve,reject)=>{
         const payLoad={
             userID
         }
-        const secret = "KET SECRET";
+        const secret = key.ACCESS_TOKEN_SECRET;
         const option = {
             expiresIn: '1h'
         }
@@ -16,6 +17,31 @@ const signAccessToken = async (userID)=>{
     })
 }
 
+const verifyAccessToken = (req,res,next)=>{
+    console.log(req.header['authorization']);
+    if (!req.header['authorization']){
+        return res.status(403).send({
+            message: `Unauthorized`
+        })
+    }
+    const authHeader = req.header['authorization'];
+    const bearerToken = authHeader.split(' ');
+    const token = bearerToken[1];
+
+    console.log(token);
+
+    JWT.verify(token, key.ACCESS_TOKEN_SECRET, (err,payLoad)=>{
+        if (err){
+            return res.status(403).send({
+                message: `Unauthorized`
+            })
+        }
+        req.payLoad = payLoad;
+        next();
+    })
+}
+
 module.exports = {
-    signAccessToken
+    signAccessToken,
+    verifyAccessToken
 }
