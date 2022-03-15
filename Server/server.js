c5233114eab0437a9e0a0b06689ad5ee15d45991
DucDatt <dat.hd3745@sinhvien.hoasen.edu.vn>
2022-03-15 18:31:26 +0700

    fix popup post music

--- Server/server.js
+++ Server/server.js
@@ -11,16 +11,18 @@ const exampleRouter = require('./routers/example.router');
 const userRouter = require('./routers/user.router');
 const audioRouter = require("./routers/audio.js");
 const artistRouter = require("./routers/artist.js");
+const typeAudioRouter = require("./routers/type-audio.js")
 
-server.use("/user",userRouter);
+server.use("/user", userRouter);
 server.use("/api", exampleRouter);
 server.use("/audio", audioRouter);
 server.use("/artist", artistRouter);
+server.use("/type", typeAudioRouter);
 
-server.use((req, res, next)=>{
+server.use((req, res, next) => {
     next(createError.NotFound(`This route does not exits`))
 })
-server.use((err, req, res, next)=>{
+server.use((err, req, res, next) => {
     res.json({
         status: err.status || 500,
         message: err.message
